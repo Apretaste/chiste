702f875d2c98e4fcb2d684efc873de86b027a507
kumahacker <kumahavana@gmail.com>
2017-10-20 01:59:07 -0400

    fix

--- service.php
+++ service.php
@@ -13,42 +13,44 @@ class Chiste extends Service {
 		$page = @file_get_contents("http://feeds.feedburner.com/ChistesD4w?format=xml");
 
 		// clean the jokes into an array
+
 		$mark = '<description>';
 		$jokes = array();
-		$last_pos = 0;
+		$last_pos = stripos($page, '<item>'); // from first item
 
-		do
-		{
-			$p1 = strpos($page, $mark, $last_pos);
-			if ($p1 !== false)
+		if ($last_pos !== false)
+			do
 			{
-				$p2 = strpos($page, '</description>', $p1);
-				$joke = substr($page, $p1 + strlen($mark), $p2 - $p1 - strlen($mark));
-				$j = $joke;				
-				$j = html_entity_decode($j, ENT_COMPAT | ENT_HTML401, 'ISO-8859-1');
-				$p = strpos($j, '<br xml:base="');
-				
-				if ($p !== false) $j = substr($j, 0, $p);
-				
-				$p = strpos($j, '<a href="http://chistes.developers4web.com/');
-				
-				if ($p !== false) $j = substr($j, 0, $p);
+				$p1 = strpos($page, $mark, $last_pos);
+				if ($p1 !== false)
+				{
+					$p2 = strpos($page, '</description>', $p1);
+					$joke = substr($page, $p1 + strlen($mark), $p2 - $p1 - strlen($mark));
+					$j = $joke;
+					$j = html_entity_decode($j, ENT_COMPAT | ENT_HTML401, 'ISO-8859-1');
+					$p = strpos($j, '<br xml:base="');
 
-				$j = str_replace("/ ", " ", $j);
-				$j = nl2br($j);
-				
-				$joke = trim($j);
-				
-				$joke = wordwrap($joke, 200, "\n");
-				
-				if (strlen($joke) > 30)
-					if (stripos($joke, ' the') == false)
-						if (stripos($joke, 'the ') == false)
-							$jokes[] = $joke;
-				
-				$last_pos = $p2;
-			}
-		} while ($p1 !== false);
+					if ($p !== false) $j = substr($j, 0, $p);
+
+					$p = strpos($j, '<a href="http://chistes.developers4web.com/');
+
+					if ($p !== false) $j = substr($j, 0, $p);
+
+					$j = str_replace("/ ", " ", $j);
+					$j = nl2br($j);
+
+					$joke = trim($j);
+
+					$joke = wordwrap($joke, 200, "\n");
+
+					if (strlen($joke) > 30)
+						if (stripos($joke, ' the') == false)
+							if (stripos($joke, 'the ') == false)
+								$jokes[] = $joke;
+
+					$last_pos = $p2;
+				}
+			} while ($p1 !== false);
 
 		// get a random joke, if no joke was found, use a default joke
 		$defaultJoke = "El rey hace un pase de visita a los soldados de guardia y al primero le pregunta: <br/>- A ver ¿por que un soldado de la guardia real tiene que cumplir su tarea ante cualquier circunstancia?! <br/>Y el soldado le responde: <br/> - Si chico, a ver porque eh?! porque eh?!";
