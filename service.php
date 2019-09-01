1b0f2af732e306a2c5ec92f26ab82675f48fb2a2
kumahacker <kumahavana@gmail.com>
2019-09-01 13:53:05 -0400

    update

--- service.php
+++ service.php
@@ -9,48 +9,56 @@ class Chiste extends Service {
 	 * */
 	public function _main(Request $request)
 	{
+	    $url = "http://feeds.feedburner.com/ChistesD4w?format=xml";
 		// get the latest jokes from the internet
-		$page = @file_get_contents("http://feeds.feedburner.com/ChistesD4w?format=xml");
+		$page = $this->getUrl($url, $info);
+        $jokes = [];
+		if ($page === false)
+        {
+            $this->utils->createAlert("[Chiste] Can not access to URL $url: ".serialize($info));
+        }
+        else
+        {
+            // clean the jokes into an array
 
-		// clean the jokes into an array
+            $mark = '<description>';
+            $last_pos = stripos($page, '<item>'); // from first item
 
-		$mark = '<description>';
-		$jokes = array();
-		$last_pos = stripos($page, '<item>'); // from first item
+            if ($last_pos !== false)
+                do
+                {
+                    $p1 = strpos($page, $mark, $last_pos);
+                    if ($p1 !== false)
+                    {
+                        $p2 = strpos($page, '</description>', $p1);
+                        $joke = substr($page, $p1 + strlen($mark), $p2 - $p1 - strlen($mark));
+                        $j = $joke;
+                        $j = html_entity_decode($j, ENT_COMPAT | ENT_HTML401, 'ISO-8859-1');
+                        $p = strpos($j, '<br xml:base="');
 
-		if ($last_pos !== false)
-			do
-			{
-				$p1 = strpos($page, $mark, $last_pos);
-				if ($p1 !== false)
-				{
-					$p2 = strpos($page, '</description>', $p1);
-					$joke = substr($page, $p1 + strlen($mark), $p2 - $p1 - strlen($mark));
-					$j = $joke;
-					$j = html_entity_decode($j, ENT_COMPAT | ENT_HTML401, 'ISO-8859-1');
-					$p = strpos($j, '<br xml:base="');
+                        if ($p !== false) $j = substr($j, 0, $p);
 
-					if ($p !== false) $j = substr($j, 0, $p);
+                        $p = strpos($j, '<a href="http://chistes.developers4web.com/');
 
-					$p = strpos($j, '<a href="http://chistes.developers4web.com/');
+                        if ($p !== false) $j = substr($j, 0, $p);
 
-					if ($p !== false) $j = substr($j, 0, $p);
+                        $j = str_replace("/ ", " ", $j);
+                        $j = nl2br($j);
 
-					$j = str_replace("/ ", " ", $j);
-					$j = nl2br($j);
+                        $joke = trim($j);
 
-					$joke = trim($j);
+                        $joke = wordwrap($joke, 200, "\n");
 
-					$joke = wordwrap($joke, 200, "\n");
+                        if (strlen($joke) > 30)
+                            if (stripos($joke, ' the') == false)
+                                if (stripos($joke, 'the ') == false)
+                                    $jokes[] = $joke;
 
-					if (strlen($joke) > 30)
-						if (stripos($joke, ' the') == false)
-							if (stripos($joke, 'the ') == false)
-								$jokes[] = $joke;
+                        $last_pos = $p2;
+                    }
+                } while ($p1 !== false);
 
-					$last_pos = $p2;
-				}
-			} while ($p1 !== false);
+        }
 
 		// get a random joke, if no joke was found, use a default joke
 		$defaultJoke = "El rey hace un pase de visita a los soldados de guardia y al primero le pregunta: <br/>- A ver ¿por que un soldado de la guardia real tiene que cumplir su tarea ante cualquier circunstancia?! <br/>Y el soldado le responde: <br/> - Si chico, a ver porque eh?! porque eh?!";
@@ -67,5 +75,50 @@ class Chiste extends Service {
 		$response->createFromTemplate("basic.tpl", array("joke" => $j));
 
 		return $response;
-	}	
+	}
+
+    /**
+     * Get remote content
+     *
+     * @param $url
+     * @param array $info
+     *
+     * @return mixed
+     */
+    private function getUrl($url, &$info = [])
+    {
+        $url = str_replace("//", "/", $url);
+        $url = str_replace("http:/","http://", $url);
+        $url = str_replace("https:/","https://", $url);
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        $default_headers = [
+            "Cache-Control" => "max-age=0",
+            "Origin" => "{$url}",
+            "User-Agent" => "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36",
+            "Content-Type" => "application/x-www-form-urlencoded"
+        ];
+
+        $hhs = [];
+        foreach ($default_headers as $key => $val)
+            $hhs[] = "$key: $val";
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $hhs);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        $html = curl_exec($ch);
+        $info = curl_getinfo($ch);
+
+        if ($info['http_code'] == 301)
+            if (isset($info['redirect_url']) && $info['redirect_url'] != $url)
+                return $this->getUrl($info['redirect_url'], $info);
+
+        curl_close($ch);
+
+        return $html;
+    }
 }
