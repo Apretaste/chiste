98403c1eb525340ae6628020d81116b6c67da9af
kumahacker <kumahavana@gmail.com>
2019-10-28 00:09:00 +0100

    update

--- service.php
+++ service.php
@@ -4,173 +4,172 @@ use Goutte\Client;
 use GuzzleHttp\Client as GuzzleClient;
 use Symfony\Component\DomCrawler\Crawler;
 
-
 class ChisteService extends ApretasteService
 {
+	private $client;
 
-    private $client;
+	/**
+	 * Function executed when the service is called
+	 *
+	 * @throws \FeedException
+	 */
+	public function _main()
+	{
+		$url = "http://feeds.feedburner.com/ChistesD4w?format=xml";
+		$rss = Feed::load($url);
 
-    /**
-     * Function executed when the service is called
-     *
-     * @throws \FeedException
-     */
-    public function _main()
-    {
-        $url = "http://feeds.feedburner.com/ChistesD4w?format=xml";
-        $rss = Feed::load($url);
+		$jokes = [];
 
-        $jokes = [];
+		foreach ($rss->item as $item) {
+			$j = (string)$item->description;
+			if (strpos($j, '<') === 0) {
+				continue;
+			}
+			$j = html_entity_decode($j);
+			$p = strpos($j, '<a ');
+			if ($p !== false) {
+				$j = substr($j, 0, $p);
+			}
+			$j = preg_replace("/\s+/", " ", $j);
+			$j = str_replace(' / ', " ", $j);
+			$j = str_replace("<br /> <br /><br />", "", $j);
+			$j = str_replace("<br /><br />", "", $j);
+			$j = trim($j);
 
-        foreach ($rss->item as $item) {
-            $j = (string)$item->description;
-            if (strpos($j, '<') === 0) {
-                continue;
-            }
-            $j = html_entity_decode($j);
-            $p = strpos($j, '<a ');
-            if ($p !== false) {
-                $j = substr($j, 0, $p);
-            }
-            $j = preg_replace("/\s+/", " ", $j);
-            $j = str_replace(' / ', " ", $j);
-            $j = str_replace("<br /> <br /><br />", "", $j);
-            $j = str_replace("<br /><br />", "", $j);
-            $j = trim($j);
+			// search joke in subpage
+			if (isset($item->link)) {
+				$crawler = $this->getCrawler($item->link);
+				$j = $crawler->filter('div.singlejoke')->html();
 
-            // search joke in subpage
-            if (isset($item->link)) {
-                $crawler = $this->getCrawler($item->link);
-                $j = $crawler->filter('div.singlejoke')->html();
+				$p = strpos($j, '</strong>');
 
-                $p = strpos($j, '</strong>');
+				if ($p !== false) {
+					$j = substr($j, $p + 9);
+				}
 
-                if ($p !== false) {
-                    $j = substr($j, $p + 9);
-                }
+				$p = strpos($j, '<div ');
 
-                $p = strpos($j, '<div ');
+				if ($p !== false) {
+					$j = substr($j, 0, $p);
+				}
 
-                if ($p !== false) {
-                    $j = substr($j, 0, $p);
-                }
+				$j = str_ireplace(['<br />', '<br/>'], "", $j);
+				$j = str_ireplace(["\n", "\r"], "", $j);
+				$secret = uniqid();
+				$j = str_replace("...", "{{$secret}}", $j);
+				$j = str_replace(".", ".\n", $j);
+				$j = str_replace("-", "\n-", $j);
+				$j = str_replace("{{$secret}}", "...", $j);
 
-                $j = str_ireplace(['<br />', '<br/>'], "", $j);
-                $j = str_ireplace(["\n", "\r"], "", $j);
-                $secret = uniqid();
-                $j = str_replace("...","{{$secret}}", $j);
-                $j = str_replace(".",".\n", $j);
-                $j = str_replace("-","\n-", $j);
-                $j = str_replace("{{$secret}}", "...", $j);
+				while (strpos($j, "\n\n") !== false) {
+					$j = str_replace("\n\n", "\n", $j);
+				}
+			}
 
-                while (strpos($j, "\n\n") !== false) {
-                    $j = str_replace("\n\n", "\n", $j);
-                }
-            }
+			if (empty($j) || strlen($j) < 25) {
+				continue;
+			}
 
-            if (empty($j) || strlen($j) < 25) {
-                continue;
-            }
+			$jokes[] = [
+				'description' => nl2br(strip_tags($j)),
+				'author'      => (string)$item->author
+			];
+		}
+		// get a random joke, if no joke was found, use a default joke
+		$defaultJoke = [
+			'description' => "El rey hace un pase de visita a los soldados de guardia y al primero le pregunta: <br/>- A ver ¿por que un soldado de la guardia real tiene que cumplir su tarea ante cualquier circunstancia?! <br/>Y el soldado le responde: <br/> - Si chico, a ver porque eh?! porque eh?!",
+			'author'      => 'El chiste de siempre'
+		];
 
-            $jokes[] = [
-                'description' => nl2br(strip_tags($j)),
-                'author'      => (string)$item->author
-            ];
-        }
-        // get a random joke, if no joke was found, use a default joke
-        $defaultJoke = [
-            'description' => "El rey hace un pase de visita a los soldados de guardia y al primero le pregunta: <br/>- A ver ¿por que un soldado de la guardia real tiene que cumplir su tarea ante cualquier circunstancia?! <br/>Y el soldado le responde: <br/> - Si chico, a ver porque eh?! porque eh?!",
-            'author'      => 'El chiste de siempre'
-        ];
+		$j = empty($jokes) ? $defaultJoke : $jokes[mt_rand(0, count($jokes) - 1)];
 
-        $j = empty($jokes) ? $defaultJoke : $jokes[mt_rand(0, count($jokes) - 1)];
+		// create response
+		$this->response->setLayout('chiste.ejs');
+		$this->response->setTemplate("basic.ejs", ["joke" => $j]);
 
-        // create response
-        $this->response->setLayout('chiste.ejs');
-        $this->response->setTemplate("basic.ejs", ["joke" => $j]);
+		Challenges::complete('view-chiste', $this->request->person->id);
+	}
 
-    }
+	/**
+	 * Get remote content
+	 *
+	 * @param       $url
+	 * @param array $info
+	 *
+	 * @return mixed
+	 */
+	private function getUrl($url, &$info = [])
+	{
+		$url = str_replace("//", "/", $url);
+		$url = str_replace("http:/", "http://", $url);
+		$url = str_replace("https:/", "https://", $url);
 
-    /**
-     * Get remote content
-     *
-     * @param       $url
-     * @param array $info
-     *
-     * @return mixed
-     */
-    private function getUrl($url, &$info = [])
-    {
-        $url = str_replace("//", "/", $url);
-        $url = str_replace("http:/", "http://", $url);
-        $url = str_replace("https:/", "https://", $url);
+		$ch = curl_init();
 
-        $ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
 
-        curl_setopt($ch, CURLOPT_URL, $url);
+		$default_headers = [
+			"Cache-Control" => "max-age=0",
+			"Origin"        => "{$url}",
+			"User-Agent"    => "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36",
+			"Content-Type"  => "application/x-www-form-urlencoded"
+		];
 
-        $default_headers = [
-            "Cache-Control" => "max-age=0",
-            "Origin"        => "{$url}",
-            "User-Agent"    => "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36",
-            "Content-Type"  => "application/x-www-form-urlencoded"
-        ];
+		$hhs = [];
+		foreach ($default_headers as $key => $val) {
+			$hhs[] = "$key: $val";
+		}
 
-        $hhs = [];
-        foreach ($default_headers as $key => $val) {
-            $hhs[] = "$key: $val";
-        }
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $hhs);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $hhs);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$html = curl_exec($ch);
+		$info = curl_getinfo($ch);
 
-        $html = curl_exec($ch);
-        $info = curl_getinfo($ch);
+		if ($info['http_code'] == 301) {
+			if (isset($info['redirect_url']) && $info['redirect_url'] != $url) {
+				return $this->getUrl($info['redirect_url'], $info);
+			}
+		}
 
-        if ($info['http_code'] == 301) {
-            if (isset($info['redirect_url']) && $info['redirect_url'] != $url) {
-                return $this->getUrl($info['redirect_url'], $info);
-            }
-        }
+		curl_close($ch);
 
-        curl_close($ch);
+		return $html;
+	}
 
-        return $html;
-    }
+	/**
+	 * Crawler client
+	 *
+	 * @return \Goutte\Client
+	 */
+	public function getClient()
+	{
+		if (is_null($this->client)) {
+			$this->client = new Client();
+			$guzzle = new GuzzleClient(["verify" => false]);
+			$this->client->setClient($guzzle);
+		}
 
-    /**
-     * Crawler client
-     *
-     * @return \Goutte\Client
-     */
-    public function getClient()
-    {
-        if (is_null($this->client)) {
-            $this->client = new Client();
-            $guzzle = new GuzzleClient(["verify" => false]);
-            $this->client->setClient($guzzle);
-        }
+		return $this->client;
+	}
 
-        return $this->client;
-    }
+	/**
+	 * Get crawler for URL
+	 *
+	 * @param string $url
+	 *
+	 * @return \Symfony\Component\DomCrawler\Crawler
+	 */
+	protected function getCrawler($url = "")
+	{
+		$url = trim($url);
+		if ($url != '' && $url[0] == '/') {
+			$url = substr($url, 1);
+		}
 
-    /**
-     * Get crawler for URL
-     *
-     * @param string $url
-     *
-     * @return \Symfony\Component\DomCrawler\Crawler
-     */
-    protected function getCrawler($url = "")
-    {
-        $url = trim($url);
-        if ($url != '' && $url[0] == '/') {
-            $url = substr($url, 1);
-        }
+		$crawler = $this->getClient()->request("GET", $url);
 
-        $crawler = $this->getClient()->request("GET", $url);
-
-        return $crawler;
-    }
+		return $crawler;
+	}
 }
