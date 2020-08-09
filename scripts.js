a6eada7d88004c183329b0eceb3039594b651063
kumahacker <kumahavana@gmail.com>
2020-08-09 08:42:18 -0300

    update

--- scripts.js
+++ scripts.js
@@ -4,3 +4,29 @@ $(document).ready(function() {
     $('select').formSelect();
     $('.sidenav').sidenav();
 });
+
+function share(){
+    apretaste.send({command:'PIZARRA PUBLICAR', data:{
+            text: $('#message').val(),
+            image: '',
+            redirect: false,
+            callback: {
+                name: 'toast',
+                data: 'Tu chiste compartido en Pizarra'
+            },
+            link: {
+                command: btoa(JSON.stringify({
+                    command: 'CHISTE VER',
+                    data: {
+                        id: jokeId
+                    }
+                })),
+                icon: 'mood',
+                text: joke.substr(0, 100)
+            }
+        }});
+}
+
+function toast(message){
+    M.toast({html: message});
+}
