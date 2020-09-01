0e8bc5262d873a40707a3d4ec81c94b5e31a6d8e
Salvi Pascual <salvi.pascual@gmail.com>
2020-09-01 10:50:32 -0400

    remove unnecesary code

--- scripts.js
+++ scripts.js
@@ -1,43 +1,40 @@
 $(document).ready(function() {
-    $('.tabs').tabs();
-    $('.modal').modal();
-    $('select').formSelect();
-    $('.sidenav').sidenav();
+	$('.modal').modal();
 });
 
 var share;
 
 function init(joke, jokeId) {
-    share = {
-        text: joke.substr(0, 100),
-        icon: 'grin-squint',
-        send: function () {
-            apretaste.send({
-                command: 'PIZARRA PUBLICAR',
-                redirect: false,
-                callback: {
-                    name: 'toast',
-                    data: 'Tu chiste fue compartido en Pizarra'
-                },
-                data: {
-                    text: $('#message').val(),
-                    image: '',
-                    link: {
-                        command: btoa(JSON.stringify({
-                            command: 'CHISTE VER',
-                            data: {
-                                id: jokeId
-                            }
-                        })),
-                        icon: share.icon,
-                        text: share.text
-                    }
-                }
-            })
-        }
-    };
+	share = {
+		text: joke.substr(0, 100),
+		icon: 'grin-squint',
+		send: function () {
+			apretaste.send({
+				command: 'PIZARRA PUBLICAR',
+				redirect: false,
+				callback: {
+					name: 'toast',
+					data: 'Tu chiste fue compartido'
+				},
+				data: {
+					text: $('#message').val(),
+					image: '',
+					link: {
+						command: btoa(JSON.stringify({
+							command: 'CHISTE VER',
+							data: {
+								id: jokeId
+							}
+						})),
+						icon: share.icon,
+						text: share.text
+					}
+				}
+			})
+		}
+	};
 }
 
 function toast(message){
-    M.toast({html: message});
+	M.toast({html: message});
 }
